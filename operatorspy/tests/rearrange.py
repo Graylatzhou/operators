7b78a7ded102b713eec1bd4502b354417b436257
qinyiqun <qinyiqun@outlook.com>
2024-12-24 03:24:12 +0000

    沐曦增加 rearrange

--- operatorspy/tests/rearrange.py
+++ operatorspy/tests/rearrange.py
@@ -104,6 +104,15 @@ def test_ascend(lib, test_cases):
         test(lib, handle, "npu", x_shape, x_stride, y_shape, y_stride)
     destroy_handle(lib, handle) 
 
+def test_maca(lib, test_cases):
+    device = DeviceEnum.DEVICE_MACA
+    handle = create_handle(lib, device)
+    for test_case in test_cases:
+        x_shape, x_stride = test_case[0]
+        y_shape, y_stride = test_case[1]
+        test(lib, handle, "cuda", x_shape, x_stride, y_shape, y_stride)
+    destroy_handle(lib, handle) 
+
 if __name__ == "__main__":
     args = get_args()
     test_cases = [
@@ -140,3 +149,5 @@ if __name__ == "__main__":
         test_bang(lib, test_cases)
     if args.ascend:
         test_ascend(lib, test_cases)
+    if args.maca:
+        test_maca(lib, test_cases)
